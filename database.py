8c2b6ee7ba587dbf615bb28a0618540c64996468
iGodShoT <comar1234567890@gmail.com>
2023-11-21 10:20:22 +0300

    Commit 2

--- database.py
+++ database.py
@@ -3,7 +3,6 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 database_url = "sqlite:///./sql_orm.db"
-
 engine = create_engine(database_url, connect_args={"check_same_thread": False})
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
